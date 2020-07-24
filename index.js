aa255336486df43863f43ee06cf898c0b296671e
会飞的肥牛 <469235606@qq.com>
2020-07-24 11:42:27 +0800

    '01'

---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * 服务器端入口文件
+ */
+//1 创建并启动web服务器
+const express = require('express');
+
+let port = 5050;/* 端口 */
+let app = express();
+app.listen(port,()=>{
+	console.log(port)
+})
+
+//2 创建前置中间件
+
+
+//3 声明路由和路由器
+
+
+//4 创建后置中间件
---
+++ pool.js
@@ -0,0 +1,14 @@
+/**
+ * Mysql 数据连接池
+ */
+
+const mysql = require('mysql');
+
+module.exports = mysql.createPool({
+	host:'127.0.0.1',
+	port:3306,
+	user:'root',
+	password:'',
+	database:'iweb',
+	connectionLimit:10
+})
